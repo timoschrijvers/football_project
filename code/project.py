4c9f1a2a046b513df09c2fdc714f9e3a9b27b0f2
Hendrikto <hendrik.to@gmail.com>
2017-04-06 13:10:20 +0200

    Execute the plan for the red car.

--- code/project.py
+++ code/project.py
@@ -1,10 +1,13 @@
 import cv2
 import yaml
+from subprocess import run
 from analyzer import Analyzer
 from planner import Planner
 from helper_functions import newest_frame
 
 # author: Hendrik Werner s4549775
+
+command = ["java", "-jar", "executor.jar"]
 
 with open("config.yml") as conf_file:
     conf = yaml.safe_load(conf_file)
@@ -25,4 +28,5 @@ while cv2.waitKey(1) != 27:
     if success:
         imageHSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         cv2.imshow("Webcam", image)
-        planner.plan(imageHSV)
+        plan = planner.plan(imageHSV)
+        run(command + plan[1].split())  # red car
